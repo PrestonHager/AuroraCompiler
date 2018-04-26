e83cc11a3c10d21a806f3176802d5eb884b535e9
Preston Hager <385428@beaverton.k12.or.us>
2018-04-26 09:11:49 -0700

    Working on lexer still.

--- aurora_lexer.py
+++ aurora_lexer.py
@@ -14,18 +14,46 @@ class AuroraLexer:
 
     def _lex(self, code):
         current_id = ""
-        index = 0
-        position = [0, 0]
-        while index < len(code):
+        index = -1
+        position = [-1, 0]
+        in_comment = False
+        in_string = False
+        while index < len(code)-1:
+            index += 1
+            position[0] += 1
+            if code[index] == "\n":
+                position[1] += 1
+                position[0] = 0
+                if in_comment:
+                    in_comment = False
+                    self.tokenized_code.append(("ID", current_id, copy(position)))
+                continue
+            if in_comment:
+                current_id += code[index]
+                continue
+            if in_string:
+                if code[index] == "\"":
+                    self.tokenized_code.append(("ID", current_id, copy(position)))
+                    current_id = ""
+                else:
+                    current_id += code[index]
+                    continue
+            continue_loop = False
             for key in self.operation_keys:
                 operation = self.operations[key]
                 if ''.join(code[index:index+len(operation)]) == operation:
+                    if current_id != "":
+                        self.tokenized_code.append(("ID", current_id, copy(position)))
+                        current_id = ""
                     self.tokenized_code.append((key, operation, copy(position)))
                     index += len(operation)
                     position[0] += len(operation)
+                    if key == "STRING_DEF":
+                        in_string = True
+                    if key == "COMMENT":
+                        in_comment = True
                     break
-            if code[index] == "\n":
-                position[1] += 1
-                position[0] = -1
-            index += 1
-            position[0] += 1
+                    continue_loop = True
+            if continue_loop:
+                continue
+            current_id += code[index]
