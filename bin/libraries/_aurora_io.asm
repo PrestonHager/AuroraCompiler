d10c64dda041c7b4baf7080c39c18cc66b368793
Preston Hager <preston@hagerfamily.com>
2020-04-03 12:51:54 -0700

    .

--- bin/libraries/_aurora_io.asm
+++ bin/libraries/_aurora_io.asm
@@ -8,3 +8,32 @@ io_out:
   .done:
   pop eax
   ret
+
+print:
+  push eax
+  push ebx
+  
+  mov si, [_aurora_arg_buffer+4]
+  mov ebx, 0x8b000
+  
+  .repeat:
+    mov al, byte [si]
+    inc si
+    cmp al, 0
+    je .done
+    mov [ebx], al
+    add ebx, 2
+    jmp .repeat
+  
+  .done:
+  pop eax
+  pop ebx
+  ret
+  
+println:
+  call print
+  mov [_aurora_arg_buffer+4], word NEWLINE
+  call print
+  ret
+  
+NEWLINE db 0x10, 0
